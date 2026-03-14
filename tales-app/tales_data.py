72ab5018a5a77136471286626abaa5890a1a14b1
XXShoker <zapomni2803@gmail.com>
2026-03-14 10:08:06 +0300

    Create file containing sample tales data structure

---
+++ tales-app/tales_data.py
@@ -0,0 +1,17 @@
+sample_tales_data = [
+    {
+        "title": "The Little Mermaid",
+        "author": "Hans Christian Andersen",
+        "content": "A tale about a mermaid who dreams of becoming human."
+    },
+    {
+        "title": "The Frog Prince",
+        "author": "Brothers Grimm",
+        "content": "A story of a princess and a cursed frog who becomes a prince."
+    },
+    {
+        "title": "Cinderella",
+        "author": "Charles Perrault",
+        "content": "A tale of a girl who overcomes hardship with the help of her magical fairy godmother."
+    }
+]
